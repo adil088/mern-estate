d06afe5dddeb5be63783bbebb982ca5c32f3befa
Mohd Adil <adil88.mohd@gmail.com>
2024-02-28 10:11:58 +0530

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api': {  // everytime it sees /api, add the target before it
+        target:'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
