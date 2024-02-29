6a8df0e9d3ac4c4bd1cc5e39aab06688b1a6751e
Mohd Adil <adil88.mohd@gmail.com>
2024-02-29 09:41:16 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import { signInFailure, signInStart, signInSuccess } from '../redux/user/userSlice'
+import userSlice from '../redux/user/userSlice'
 
 const SignIn = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch()
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false)
-  const [created, setCreated] = useState('')
+  const {loading, error} = useSelector((state)=> state.user);
 
   const handleChange = (e)=>{
     setFormData(
@@ -19,7 +21,7 @@ const SignIn = () => {
   const handleSubmit= async(e)=>{
     e.preventDefault();
     try {
-      setLoading(true)
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin',  // see vite.config.js
       {
         method: 'POST',
@@ -31,17 +33,13 @@ const SignIn = () => {
       });
       const data = await res.json();
       if(data.success === false){
-        setError(data.message);
-        setLoading(false)
+        dispatch(signInFailure(data.message))
         return
       }
-      setLoading(false)  
-      setCreated('User created successfully')
-      setError(null)    
-      navigate('/home')
+      dispatch(signInSuccess(data))    
+      navigate('/')
     } catch (error) {
-      setLoading(false)
-      setError(error.message)
+      dispatch(signInFailure(error.message))
     }
   }
 
