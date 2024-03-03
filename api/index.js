4f6890d96db39422e7cbd51092d0cf3881fe3333
Mohd Adil <adil88.mohd@gmail.com>
2024-03-03 15:50:47 +0530

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 dotenv.config();
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
+import listingRouter from './routes/listing.route.js'
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("Connected to MongoDB!")
@@ -23,6 +24,7 @@ app.listen(3000, ()=>{
 
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
+app.use("/api/listing", listingRouter)
 
 
 app.use((err, req, res, next)=>{
