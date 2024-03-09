f240c4428d64332c224f5d088fde34ebb49b4c13
Mohd Adil <adil88.mohd@gmail.com>
2024-03-09 23:21:25 +0530

    complete about page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -56,7 +56,7 @@ const Home = () => {
           <br /> place with ease
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
-          Home Harbour is the best place to find your next perfect place to
+          Home Harbor is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
